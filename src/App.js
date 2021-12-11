bdeefd63a6104eb08bd4997c4f8c2e8b870e83eb
emorilebo <emorylebo@gmail.com>
2021-12-12 00:54:40 +0100

    ADD index.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
 import {CTA, Brand, Navbar} from './components'
 import './App.css'
+import './index.css'
 
 const App = () => {
   return (
