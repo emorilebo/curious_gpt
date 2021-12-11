a859eeaa03e2299050aa05b194d505738f941a14
emorilebo <emorylebo@gmail.com>
2021-12-11 23:51:18 +0100

    UPDATE container files

---
+++ src/containers/index.jsx
@@ -0,0 +1,6 @@
+export { default as Blog} from './blog/Blog';
+export { default as Features} from './features/Features';
+export { default as Blog} from './blog/Blog';
+export { default as Blog} from './blog/Blog';
+export { default as Blog} from './blog/Blog';
+export { default as Blog} from './blog/Blog';
