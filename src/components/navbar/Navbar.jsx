0b74eee03ceb1ca6e65ae2001ad12faccc736eb0
emorilebo <emorylebo@gmail.com>
2021-12-12 06:47:15 +0100

    UPDATE navbar with links sign in sing up

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,7 +18,10 @@ const Navbar = () => {
                 <p><a href="#features">Case Studies</a> </p>
                 <p><a href="#blog">Library</a> </p>
             </div>
-
+            </div>
+            <div className="gpt3__navbar-sign">
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
             </div>
         </div>
     )
